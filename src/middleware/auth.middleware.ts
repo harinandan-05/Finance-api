62bef2e7adf14791191865f64d1b43361f0f3c41
Harinandan <harinandannn@gmail.com>
2026-04-05 01:35:12 +0530

    auth middlware done

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { Request , Response , NextFunction } from "express";
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = async (req:Request ,res:Response,next:NextFunction) =>{
+    try{
+        const authHeader = req.headers.authorization
+        if(!authHeader || !authHeader.startsWith("Bearer")){
+            return res.status(401).json({msg:"no token provided"})
+        }
+
+        const token = authHeader.split("")[1]
+        const decoded = jwt.verify(token,process.env.JWT_SECRET!) as {
+            id: string,
+            role:string
+        }
+
+        (req as any).user = decoded
+        next()
+    }catch(err){
+        res.status(500).json({msg:"invalid token"})
+    }
+}
